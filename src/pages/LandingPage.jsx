270f8d1586100cf0163db6c408d1a5555974d160
elmergustavo <elmergustavo79@gmail.com>
2022-08-20 14:24:47 -0600

    login

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,23 +1,62 @@
-import React from 'react'
-import ImagenPrincipal from '../Assets/images/landing.svg'
+import React from "react";
+import ImagenPrincipal from "../Assets/images/landing.svg";
 
 const LandingPage = () => {
-    return (
-        <div className='font-bold text-lg'>
-            <img className='flex justify-center' src={ImagenPrincipal}/>
+  return (
+    <div className="block text-center rounded-lg md:grid md:grid-cols-1/5col">
+      <div className="flex justify-center">
+        <img className="" src={ImagenPrincipal} />
+      </div>
 
-
-            <div className='grid justify-center'>
-            <h1>Hola, ¿Qué tal?</h1>
-            <h5>Bienvenido docente a Atenea, su nueva herramienta de apoyo académico</h5>
+      <div className="container_right flex flex-col justify-center bg-white h-screen p-5">
+        <h3 className="text-4xl font-bold text-center">!Hola de Nuevo!</h3>
+        <form action="" class="h-screen flex flex-col p-12 gap-5">
+          <h3 class="font-medium">
+            Como ha estado?, es un gusto vovler a tenerlo por aca en Atenea
+          </h3>
+          <div class="flex flex-col items-start gap-1">
+            <label className="font-medium text-[#7064ff]" for="email">
+              Usuario / Correo
+            </label>
+            <input
+              className="w-full bg-blue-50 p-2 rounded-md text-sm outline-none"
+              type="email"
+              name="email"
+              placeholder="please Enter your email"
+            />
+          </div>
+          <div class="flex flex-col items-start gap-1">
+            <label class="font-medium  text-[#7064ff]" for="password">
+              Contraseña
+            </label>
+            <input
+              class="w-full bg-blue-50 p-2 rounded-md outline-none text-sm"
+              type="password"
+              name="password"
+              placeholder="please Enter your password"
+            />
+          </div>
+          <div class="remember_password flex justify-between w-full">
+            <label class="remember_password_left flex items-center gap-1 cursor-pointer">
+              <input type="checkbox" />
+              <span class="text-sm">remember password</span>
+            </label>
+            <div class="remember_password_right">
+              <a class="text-[#7064ff] text-sm" href="">
+                Forget Password?
+              </a>
             </div>
+          </div>
+          <div>
+            <button className="w-full bg-[#7064ff] text-white rounded-md p-2 transition-all duration-300 ease-in-out hover:bg-blue-400">
+              Iniciar sesion
+            </button>
+          </div>
+        </form>
+      
+      </div>
+    </div>
+  );
+};
 
-            <div className='grid justify-center justify-items-start w-full'>
-            <button className='rounded-full text-center w-full bg-[#7064FF]'>Ingresar</button>
-            <button className='rounded-full text-center w-full bg-[#7064FF]'>Registrarse</button>
-            </div>
-        </div>
-    )
-}
-
-export { LandingPage }  
+export { LandingPage };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,35 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSuplight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        lightGray: '#F5F5F5'
+      } , 
+      gridTemplateColumns : {
+        //change style and variable
+        // 'variable' : 'your assets exp : '1fr 2fr' exp2 : 'reapet(auto-fit , minmax(10rem , 1fr))'
+        // to use add this class name : grid-cols-4col
+        '1/5col' : '1.5fr 1fr' , 
+        '4col' : '4fr 1fr' , 
+      } ,
+      gridTemplateRows : {
+        //change style and variable
+        // 'variable' : 'your assets exp : '1fr 2fr' exp2 : 'reapet(auto-fit , minmax(10rem , 1fr))'
+        // to use add this class name : grid-rows-row12
+        'row12' : '1fr 2fr' , 
+      } ,
+    },
   },
   plugins: [],
 }
